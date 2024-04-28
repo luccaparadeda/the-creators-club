34fbb39fc986b45a2b841046ffae36406790342c
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-04-28 20:18:22 -0300

    feat: finished website

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"background-1-mob": 'url("/background-section.png")',
+				"background-2-mob": 'url("/last-background-image-mob.png")',
+				"background-1-desk": 'url("/background-section-desk.jpg")',
+				"background-2-desk": 'url("/last-background-image-desk.jpg")',
+			},
+			boxShadow: {
+				"offwhite-top": "0 -10px 10px #F8F8F8",
+			},
+		},
+	},
+	plugins: [],
+};
